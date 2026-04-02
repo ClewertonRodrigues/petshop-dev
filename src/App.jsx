75d820e25c4c9ca6a21f9fd1de3172c45cad92ab
clewertondev <clewertonrodrigues93819@gmail.com>
2026-04-02 18:40:17 -0300

    feat: implementa hero section e ajusta container para layout responsivo

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,39 @@
-import { useState } from 'react'
-import styles from "./styles.module.scss"
+import styles from "./styles.module.scss";
 
-import { Container } from './components/container';
-import { Header } from './components/header';
+import { Container } from "./components/container";
+import { Header } from "./components/header";
+
+import imgInicio from "./assets/services-4.png";
 
 function App() {
-
   return (
     <>
-      <Header/>
-      <Container>
-          
-      </Container>
+      <Header />
+      <section className={styles.container}>
+        <Container>
+          <div className={styles.containerContent}>
+            <div className={styles.content}>
+              <h1>
+                Cuidado e <span>Amor</span> para seu Pet
+              </h1>
+              <p>
+                Oferecemos serviços completos de pet shop, banho , tosa,
+                veterinário e muito mais. Seu melhor amigo merece o melhor
+                cuidado!
+              </p>
+
+              <div className={styles.boxButtons}>
+                <a className={styles.btnAction}>Conhecer Serviços</a>
+                <a className={styles.btnContact}>Fale Conosco</a>
+              </div>
+            </div>
+
+            <img className={styles.img} src={imgInicio} alt="" />
+          </div>
+        </Container>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
