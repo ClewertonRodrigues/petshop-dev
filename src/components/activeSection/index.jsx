a06f69b9ffdf1894e22ef3b4d89371577b7e5e0b
clewertondev <clewertonrodrigues93819@gmail.com>
2026-04-20 16:16:42 -0300

    feat: implementa detecção de seção ativa com IntersectionObserver

---
+++ src/components/activeSection/index.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+
+export function useActiveSection() {
+  const [activeSection, setActiveSection] = useState("");
+  const thresholdValue = window.innerWidth > 1024 ? 0.7 : 0.3;
+
+  useEffect(() => {
+    const sections = document.querySelectorAll("section");
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setActiveSection(entry.target.id);
+          }
+        });
+      },
+      {
+        threshold: thresholdValue,
+      },
+    );
+
+    sections.forEach((section) => {
+      observer.observe(section);
+    });
+
+    return () => {
+      sections.forEach((section) => {
+        observer.unobserve(section);
+      });
+    };
+  }, []);
+
+  return activeSection;
+}
